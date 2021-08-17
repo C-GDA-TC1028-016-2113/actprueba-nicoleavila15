1b4a52ec5ae4534e287df23dcb48233767ff18a7
nicoleavila15 <A01640843@itesm.mx>
2021-08-17 17:14:05 +0000

    Actividad enviada

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+   num1= int(input("dame un número"))
+   num2= int(input("dame un número"))
+   suma= num1+num2
+   resta= num1-num2
+   mult= num1*num2
+   print("Suma", suma)
+   print("Resta", resta)
+   print("Multiplicación", mult)
+
 
 if __name__=='__main__':
     main()
--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    num1= int(input("Dame un número: "))
+    num2= int(input("Dame un número: "))
+    suma= num1+num2
+    resta= num1-num2
+    mult= num1*num2
+    print("Suma:", suma)
+    print("Resta:", resta)
+    print("Multiplicación:", mult)
 
 
 if __name__ == '__main__':
