1e8b90ccfc3859a0ba85d5ada933a7d1b1b88547
nicoleavila15 <A01640843@itesm.mx>
2021-08-17 20:28:58 +0000

    Envio 2

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,13 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-   num1= int(input("dame un número"))
-   num2= int(input("dame un número"))
-   suma= num1+num2
-   resta= num1-num2
-   mult= num1*num2
-   print("Suma", suma)
-   print("Resta", resta)
-   print("Multiplicación", mult)
+    print("Hello World!")
+    
+
 
 
 if __name__=='__main__':
